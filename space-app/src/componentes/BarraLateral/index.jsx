bd866ecffa7335d47fc49501f560047d86b623f8
grbalmeida <g.r.almeida@live.com>
2023-10-14 08:55:00 -0300

    Estrutura do item de navegação

--- space-app/src/componentes/BarraLateral/index.jsx
+++ space-app/src/componentes/BarraLateral/index.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import ItemNavegacao from './ItemNavegacao';
+
 const ListaEstilizada = styled.ul`
     list-style: none;
     padding: 0;
@@ -12,12 +14,19 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href="#">Início</a>
-                    </li>
-                    <li>
-                        <a href="#">Mais curtidos</a>
-                    </li>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/home-ativo.png"
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo="/icones/mais-vistas-ativo.png"
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
